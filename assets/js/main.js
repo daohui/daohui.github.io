3d1593463191e3a9e671cc58132e2d4ebade0691
Daohui Li <daohui2000@outlook.com>
2017-08-24 20:22:33 -0500

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,15 +4,15 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
-(function($) {
+(function ($) {
 
 	var settings = {
 
 		// Parallax background effect?
-			parallax: true,
+		parallax: true,
 
 		// Parallax factor (lower = more intense, higher = less intense).
-			parallaxFactor: 20
+		parallaxFactor: 20
 
 	};
 
@@ -24,7 +24,7 @@
 		xsmall: '(max-width: 480px)'
 	});
 
-	$(function() {
+	$(function () {
 
 		var $window = $(window),
 			$body = $('body'),
@@ -33,102 +33,81 @@
 			$main = $('#main');
 
 		// Disable animations/transitions until the page has loaded.
-			$body.addClass('is-loading');
+		$body.addClass('is-loading');
 
-			$window.on('load', function() {
-				$body.removeClass('is-loading');
-			});
+		$window.on('load', function () {
+			$body.removeClass('is-loading');
+		});
 
 		// Touch?
-			if (skel.vars.mobile) {
+		if (skel.vars.mobile) {
 
-				// Turn on touch mode.
-					$body.addClass('is-touch');
+			// Turn on touch mode.
+			$body.addClass('is-touch');
 
-				// Height fix (mostly for iOS).
-					window.setTimeout(function() {
-						$window.scrollTop($window.scrollTop() + 1);
-					}, 0);
+			// Height fix (mostly for iOS).
+			window.setTimeout(function () {
+				$window.scrollTop($window.scrollTop() + 1);
+			}, 0);
 
-			}
+		}
 
 		// Fix: Placeholder polyfill.
-			$('form').placeholder();
+		$('form').placeholder();
 
 		// Prioritize "important" elements on medium.
-			skel.on('+medium -medium', function() {
-				$.prioritize(
-					'.important\\28 medium\\29',
-					skel.breakpoint('medium').active
-				);
-			});
+		skel.on('+medium -medium', function () {
+			$.prioritize(
+				'.important\\28 medium\\29',
+				skel.breakpoint('medium').active
+			);
+		});
 
 		// Footer.
-			skel.on('+medium', function() {
-				$footer.insertAfter($main);
-			});
+		skel.on('+medium', function () {
+			$footer.insertAfter($main);
+		});
 
-			skel.on('-medium !medium', function() {
-				$footer.appendTo($header);
-			});
+		skel.on('-medium !medium', function () {
+			$footer.appendTo($header);
+		});
 
 		// Header.
 
-			// Parallax background.
+		// Parallax background.
 
-				// Disable parallax on IE (smooth scrolling is jerky), and on mobile platforms (= better performance).
-					if (skel.vars.browser == 'ie'
-					||	skel.vars.mobile)
-						settings.parallax = false;
+		// Disable parallax on IE (smooth scrolling is jerky), and on mobile platforms (= better performance).
+		if (skel.vars.browser == 'ie'
+			|| skel.vars.mobile)
+			settings.parallax = false;
 
-				if (settings.parallax) {
+		if (settings.parallax) {
 
-					skel.on('change', function() {
+			skel.on('change', function () {
 
-						if (skel.breakpoint('medium').active) {
+				if (skel.breakpoint('medium').active) {
 
-							$window.off('scroll.strata_parallax');
-							$header.css('background-position', 'top left, center center');
+					$window.off('scroll.strata_parallax');
+					$header.css('background-position', 'top left, center center');
 
-						}
-						else {
+				}
+				else {
 
-							$header.css('background-position', 'left 0px');
+					$header.css('background-position', 'left 0px');
 
-							$window.on('scroll.strata_parallax', function() {
-								$header.css('background-position', 'left ' + (-1 * (parseInt($window.scrollTop()) / settings.parallaxFactor)) + 'px');
-							});
-
-						}
-
-					});
-
-					$window.on('load', function() {
-						$window.triggerHandler('scroll');
+					$window.on('scroll.strata_parallax', function () {
+						$header.css('background-position', 'left ' + (-1 * (parseInt($window.scrollTop()) / settings.parallaxFactor)) + 'px');
 					});
 
 				}
 
-		// Main Sections: Two.
+			});
 
-			// Lightbox gallery.
-				$window.on('load', function() {
+			$window.on('load', function () {
+				$window.triggerHandler('scroll');
+			});
 
-					$('#two').poptrox({
-						caption: function($a) { return $a.next('h3').text(); },
-						overlayColor: '#2c2c2c',
-						overlayOpacity: 0.85,
-						popupCloserText: '',
-						popupLoaderText: '',
-						selector: '.work-item a.image',
-						usePopupCaption: true,
-						usePopupDefaultStyling: false,
-						usePopupEasyClose: false,
-						usePopupNav: true,
-						windowMargin: (skel.breakpoint('small').active ? 0 : 50)
-					});
-
-				});
+		}
 
 	});
 
